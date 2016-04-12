f7153021794f56345377e0de9b7d6233c6b37142
ArunJayan <arunjayan32@gmail.com>
2016-04-12 22:07:52 +0530

    Modified comments , we can access the second paragraph

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 #Example Web Scrapping using BeautifulSoap
 #Its a popular library used for Web Scrapping 
+#Author : Arun Jayan
 
 from bs4 import BeautifulSoup
 
@@ -14,13 +15,15 @@ print soup.prettify()
 print "============================================================="
 print soup.html.body #prints the body
 print "============================================================="
-print soup.html.title
+print soup.html.title #we wil get title 
 print "============================================================="
-print soup.body.p.next.next.string
+print soup.body.p.next.next.string 
 print "============================================================="
-print soup.get_text("|",strip=True)
+print soup.get_text("|",strip=True) #we will get all text in the webpage 
 print "============================================================="
-print soup.body.p.next.string.strip()
-print soup.body.p.next.next.string.strip()
+print soup.body.p.next.string.strip() #we will get first sentence 
+print soup.body.p.next.next.string.strip()  # we willget socond
 print "============================================================="
-
+print soup.body.p.nextSibling.nextSibling.next.next.string.strip() #by using nextSibling we will get the next paragraph
+print soup.body.p.nextSibling.nextSibling.next.next.next.next.string.strip() #
+print "============================================================="
